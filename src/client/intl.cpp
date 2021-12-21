d59e01f08433a317184d757f5c46cd87fb06be5a
Salvo Miosi <salvo.ilmiosi@gmail.com>
2021-12-21 03:33:13 +0100

    ottimizzate stringhe di traduzione

--- src/client/intl.cpp
+++ src/client/intl.cpp
@@ -1,16 +1,12 @@
 #include "intl.h"
-#include "common/resource.h"
-#include "common/svstream.h"
 
-#include <map>
-
-DECLARE_RESOURCE(locale_en_txt)
-DECLARE_RESOURCE(locale_it_txt)
+#include "locales/locale_en.txt.h"
+#include "locales/locale_it.txt.h"
 
 namespace intl {
     DEFINE_ENUM_DATA_IN_NS(intl, language,
-        (english, +[]{ return GET_RESOURCE(locale_en_txt); })
-        (italian, +[]{ return GET_RESOURCE(locale_it_txt); })
+        (english, locale_en_txt)
+        (italian, locale_it_txt)
     )
 }
 
@@ -44,34 +40,24 @@ namespace intl {
 
 #endif
 
-static const auto resource_stream(const resource_view &view) {
-    return util::isviewstream({view.data, view.length});
-}
-
 namespace intl {
-    static const auto strings = []{
-        std::map<std::string, std::string, std::less<>> strings;
-
-        auto stream = resource_stream(enums::get_data(get_system_language())());
-
-        std::string line;
-        while (std::getline(stream, line)) {
-            if (line.empty()) continue;
-            size_t space_pos = line.find_first_of(" \t");
-            size_t not_space_pos = line.find_first_not_of(" \t", space_pos);
-            strings.emplace(line.substr(0, space_pos), line.substr(not_space_pos, line.size() - 1 - not_space_pos));
-        }
-
-        return strings;
-    }();
+    static const language current_language = get_system_language();
 
     std::string translate(std::string_view str) {
-        auto it = strings.find(str);
-        if (it == strings.end()) {
-            return std::string(str);
-        } else {
-            return it->second;
-        }
+        constexpr auto lut = []<language ... Es>(enums::enum_sequence<Es...>) {
+            return std::array {
+                +[](std::string_view str) {
+                    constexpr auto strings = enums::enum_data_v<Es>;
+                    auto it = strings.find(str);
+                    if (it == strings.end()) {
+                        return str;
+                    } else {
+                        return it->second;
+                    }
+                } ...
+            };
+        }(enums::make_enum_sequence<language>());
+        return std::string(lut[enums::indexof(current_language)](str));
     }
 
     std::string format(const std::string &format_str, const std::vector<std::string> &args) {
