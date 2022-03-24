4c2d126c7df792e4da2b61a23d3c5793189476f6
Salvo Miosi <salvo.ilmiosi@gmail.com>
2022-03-24 20:17:35 +0100

    animazioni in TU separato

---
+++ src/client/game/animations.cpp
@@ -0,0 +1,136 @@
+#include "animations.h"
+
+namespace banggame {
+
+    float ease_in_out_pow(float exp, float x) {
+        return x < 0.5f ? std::pow(2.f * x, exp) / 2.f : 1.f - std::pow(-2.f * x + 2.f, exp) / 2.f;
+    }
+
+    void card_move_animation::add_move_card(card_view *card) {
+        if (std::ranges::find(data, card, &decltype(data)::value_type::first) == data.end()) {
+            data.emplace_back(card, card->get_pos());
+        }
+    }
+
+    void card_move_animation::end() {
+        for (auto &[card, _] : data) {
+            card->set_pos(card->pile->get_position_of(card));
+            card->animating = false;
+        }
+    }
+
+    void card_move_animation::do_animation_impl(float amt) {
+        for (auto &[card, start] : data) {
+            card->animating = true;
+            sdl::point dest = card->pile->get_position_of(card);
+            card->set_pos(sdl::point{
+                (int) std::lerp(start.x, dest.x, amt),
+                (int) std::lerp(start.y, dest.y, amt)
+            });
+        }
+    }
+
+    void card_move_animation::render(sdl::renderer &renderer) {
+        for (auto &[card, _] : data) {
+            card->render(renderer, false);
+        }
+    }
+
+    void card_flip_animation::end() {
+        card->flip_amt = float(!flips);
+        card->animating = false;
+    }
+
+    void card_flip_animation::do_animation_impl(float amt) {
+        card->animating = true;
+        card->flip_amt = flips ? 1.f - amt : amt;
+    }
+
+    void card_flip_animation::render(sdl::renderer &renderer) {
+        card->render(renderer, false);
+    }
+
+    void deck_shuffle_animation::end() {
+        for (card_view *card : *cards) {
+            card->animating = false;
+            card->flip_amt = 0.f;
+            card->set_pos(cards->get_pos());
+        }
+    }
+
+    void deck_shuffle_animation::do_animation(float x) {
+        const float off = options.shuffle_deck_offset;
+        const float diff = off / cards->size();
+        const float m = 1.f / (1.f - off);
+        
+        float n = off;
+        for (card_view *card : *cards) {
+            const float amt = ease_in_out_pow(options.easing_exponent, std::clamp(m * (x - n), 0.f, 1.f));
+            card->animating = true;
+            card->flip_amt = 1.f - amt;
+            card->set_pos(sdl::point{
+                (int) std::lerp(start_pos.x, cards->get_pos().x, amt),
+                (int) std::lerp(start_pos.y, cards->get_pos().y, amt)
+            });
+            n -= diff;
+        }
+    }
+
+    void deck_shuffle_animation::render(sdl::renderer &renderer) {
+        for (card_view *card : *cards) {
+            card->render(renderer, false);
+        }
+    }
+
+    void card_tap_animation::end() {
+        card->animating = false;
+        card->rotation = taps ? 90.f : 0.f;
+    }
+
+    void card_tap_animation::do_animation_impl(float amt) {
+        card->animating = true;
+        card->rotation = 90.f * (taps ? amt : 1.f - amt);
+    }
+
+    void card_tap_animation::render(sdl::renderer &renderer) {
+        card->render(renderer, false);
+    }
+
+    void player_hp_animation::do_animation_impl(float amt) {
+        player->set_hp_marker_position(std::lerp(hp_from, hp_to, amt));
+    }
+
+    void cube_move_animation::render(sdl::renderer &renderer) {
+        cube->render(renderer, false);
+    }
+
+    void cube_move_animation::do_animation_impl(float amt) {
+        sdl::point dest{};
+        if (cube->owner) dest = cube->owner->get_pos();
+        cube->animating = true;
+        cube->pos = sdl::point{
+            (int) std::lerp(start.x, dest.x + diff.x, amt),
+            (int) std::lerp(start.y, dest.y + diff.y, amt)
+        };
+    }
+
+    void cube_move_animation::end() {
+        sdl::point dest{};
+        if (cube->owner) dest = cube->owner->get_pos();
+        cube->pos = sdl::point{dest.x + diff.x, dest.y + diff.y};
+        cube->animating = false;
+    }
+
+    void pause_animation::render(sdl::renderer &renderer) {
+        card->render(renderer, false);
+    }
+
+    void pause_animation::do_animation(float) {
+        card->animating = true;
+    }
+
+    void pause_animation::end() {
+        card->animating = false;
+    }
+
+}
--- src/client/game/animations.h
+++ src/client/game/animations.h
@@ -5,9 +5,7 @@
     
 namespace banggame {
 
-    inline float ease_in_out_pow(float exp, float x) {
-        return x < 0.5f ? std::pow(2.f * x, exp) / 2.f : 1.f - std::pow(-2.f * x + 2.f, exp) / 2.f;
-    }
+    float ease_in_out_pow(float exp, float x);
 
     template<typename T>
     struct easing_animation {
@@ -21,35 +19,11 @@ namespace banggame {
 
         card_move_animation() = default;
 
-        void add_move_card(card_view *card) {
-            if (std::ranges::find(data, card, &decltype(data)::value_type::first) == data.end()) {
-                data.emplace_back(card, card->get_pos());
-            }
-        }
+        void add_move_card(card_view *card);
 
-        void end() {
-            for (auto &[card, _] : data) {
-                card->set_pos(card->pile->get_position_of(card));
-                card->animating = false;
-            }
-        }
-
-        void do_animation_impl(float amt) {
-            for (auto &[card, start] : data) {
-                card->animating = true;
-                sdl::point dest = card->pile->get_position_of(card);
-                card->set_pos(sdl::point{
-                    (int) std::lerp(start.x, dest.x, amt),
-                    (int) std::lerp(start.y, dest.y, amt)
-                });
-            }
-        }
-
-        void render(sdl::renderer &renderer) {
-            for (auto &[card, _] : data) {
-                card->render(renderer, false);
-            }
-        }
+        void end();
+        void do_animation_impl(float amt);
+        void render(sdl::renderer &renderer);
     };
 
     struct card_flip_animation : easing_animation<card_flip_animation> {
@@ -59,19 +33,9 @@ namespace banggame {
         card_flip_animation(card_view *card, bool flips)
             : card(card), flips(flips) {}
 
-        void end() {
-            card->flip_amt = float(!flips);
-            card->animating = false;
-        }
-
-        void do_animation_impl(float amt) {
-            card->animating = true;
-            card->flip_amt = flips ? 1.f - amt : amt;
-        }
-
-        void render(sdl::renderer &renderer) {
-            card->render(renderer, false);
-        }
+        void end();
+        void do_animation_impl(float amt);
+        void render(sdl::renderer &renderer);
     };
 
     struct deck_shuffle_animation {
@@ -81,35 +45,9 @@ namespace banggame {
         deck_shuffle_animation(card_pile_view *cards, sdl::point start_pos)
             : cards(cards), start_pos(start_pos) {}
 
-        void end() {
-            for (card_view *card : *cards) {
-                card->flip_amt = 0.f;
-                card->animating = false;
-            }
-        }
-
-        void do_animation(float x) {
-            size_t i = 0;
-            for (card_view *card : *cards) {
-                const float n = cards->size();
-                const float off = options.shuffle_deck_offset;
-                const float y = (x - ((n - i) / n) * off) / (1 - off);
-                const float amt = ease_in_out_pow(options.easing_exponent, std::clamp(y, 0.f, 1.f));
-                card->animating = true;
-                card->flip_amt = 1.f - amt;
-                card->set_pos(sdl::point{
-                    (int) std::lerp(start_pos.x, cards->get_pos().x, amt),
-                    (int) std::lerp(start_pos.y, cards->get_pos().y, amt)
-                });
-                ++i;
-            }
-        }
-
-        void render(sdl::renderer &renderer) {
-            for (card_view *card : *cards) {
-                card->render(renderer, false);
-            }
-        }
+        void end();
+        void do_animation(float x);
+        void render(sdl::renderer &renderer);
     };
 
     struct card_tap_animation : easing_animation<card_tap_animation> {
@@ -119,9 +57,9 @@ namespace banggame {
         card_tap_animation(card_view *card, bool taps)
             : card(card), taps(taps) {}
 
-        void do_animation_impl(float amt) {
-            card->rotation = 90.f * (taps ? amt : 1.f - amt);
-        }
+        void end();
+        void do_animation_impl(float amt);
+        void render(sdl::renderer &renderer);
     };
 
     struct player_hp_animation : easing_animation<player_hp_animation> {
@@ -133,9 +71,7 @@ namespace banggame {
         player_hp_animation(player_view *player, int hp_from)
             : player(player), hp_from(hp_from), hp_to(player->hp) {}
 
-        void do_animation_impl(float amt) {
-            player->set_hp_marker_position(std::lerp(hp_from, hp_to, amt));
-        }
+        void do_animation_impl(float amt);
     };
 
     struct cube_move_animation : easing_animation<cube_move_animation> {
@@ -144,31 +80,11 @@ namespace banggame {
         sdl::point diff;
 
         cube_move_animation(cube_widget *cube, sdl::point diff)
-            : cube(cube), diff(diff)
-        {
-            start = cube->pos;
-            cube->animating = true;
-        }
+            : cube(cube), start(cube->pos), diff(diff) {}
 
-        void render(sdl::renderer &renderer) {
-            cube->render(renderer, false);
-        }
-        
-        void do_animation_impl(float amt) {
-            sdl::point dest{};
-            if (cube->owner) dest = cube->owner->get_pos();
-            cube->pos = sdl::point{
-                (int) std::lerp(start.x, dest.x + diff.x, amt),
-                (int) std::lerp(start.y, dest.y + diff.y, amt)
-            };
-        }
-
-        void end() {
-            sdl::point dest{};
-            if (cube->owner) dest = cube->owner->get_pos();
-            cube->pos = sdl::point{dest.x + diff.x, dest.y + diff.y};
-            cube->animating = false;
-        }
+        void end();
+        void do_animation_impl(float amt);
+        void render(sdl::renderer &renderer);
     };
 
     struct pause_animation {
@@ -176,17 +92,9 @@ namespace banggame {
 
         pause_animation(card_view *card) : card(card) {}
 
-        void render(sdl::renderer &renderer) {
-            card->render(renderer, false);
-        }
-
-        void do_animation(float) {
-            card->animating = true;
-        }
-
-        void end() {
-            card->animating = false;
-        }
+        void end();
+        void do_animation(float);
+        void render(sdl::renderer &renderer);
     };
 }
 
