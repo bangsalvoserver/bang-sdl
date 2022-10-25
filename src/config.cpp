b63f154bfebf1e94813afb2cd1927bb4682c7395
Salvo Miosi <salvo.ilmiosi@gmail.com>
2022-10-25 18:46:38 +0200

    fix config

---
+++ src/config.cpp
@@ -0,0 +1,31 @@
+#include "config.h"
+
+static const std::filesystem::path filename = std::filesystem::path(SDL_GetPrefPath(nullptr, "bang-sdl")) / "config.json";
+
+void config::load() {
+    std::ifstream ifs{filename};
+    if (ifs.fail()) {
+#ifdef OFFICIAL_BANG_SERVER
+        recent_servers.push_back(OFFICIAL_BANG_SERVER);
+#endif
+        return;
+    }
+    try {
+        Json::Value value;
+        ifs >> value;
+        *this = json::deserialize<config>(value);
+        if (!profile_image.empty() && !profile_image_data) {
+            profile_image_data = widgets::profile_pic::scale_profile_image(sdl::surface(resource(profile_image)));
+        }
+    } catch (const Json::RuntimeError &) {
+        // ignore
+    } catch (const std::runtime_error &) {
+        profile_image.clear();
+        profile_image_data.reset();
+    }
+}
+
+void config::save()  {
+    std::ofstream ofs{filename};
+    ofs << json::serialize(*this);
+}
--- src/config.h
+++ src/config.h
@@ -21,36 +21,9 @@ DEFINE_STRUCT(config,
     (banggame::game_options, options)
     (bool, allow_unofficial_expansions)
     (uint16_t, server_port),
-
-    static inline const std::filesystem::path filename = std::filesystem::path(SDL_GetPrefPath(nullptr, "bang-sdl")) / "config.json";
     
-    void load() {
-        std::ifstream ifs{filename};
-        if (ifs.fail()) {
-#ifdef OFFICIAL_BANG_SERVER
-            recent_servers.push_back(OFFICIAL_BANG_SERVER);
-#endif
-            return;
-        }
-        try {
-            Json::Value value;
-            ifs >> value;
-            *this = json::deserialize<config>(value);
-            if (!profile_image.empty() && !profile_image_data) {
-                profile_image_data = widgets::profile_pic::scale_profile_image(sdl::surface(resource(profile_image)));
-            }
-        } catch (const Json::RuntimeError &) {
-            // ignore
-        } catch (const std::runtime_error &) {
-            profile_image.clear();
-            profile_image_data.reset();
-        }
-    }
-
-    void save() {
-        std::ofstream ofs{filename};
-        ofs << json::serialize(*this);
-    }
+    void load();
+    void save();
 )
 
 #endif
